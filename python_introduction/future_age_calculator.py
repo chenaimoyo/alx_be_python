9c9b5f1f4ab8eaad1961775898bf029d02640d35
Gamuchirai Mupopa <chenaimoyomupopa@gmail.com>
2024-12-11 09:48:22 -0800

    Added task 4 age in 2050

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = input("How old are you")
+current_year = 2023
+future = 2050
+gap = future - current_year
+age_2050 = current_age + gap
+
+print(f"In 2050, you will be {age_2050} years old.")
